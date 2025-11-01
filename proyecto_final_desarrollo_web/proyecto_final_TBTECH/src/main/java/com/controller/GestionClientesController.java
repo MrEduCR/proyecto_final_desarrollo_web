9a3cc6054c9a839e4d278a35614f4a8488375d1e
Jeff <159392804+TheJeff182@users.noreply.github.com>
2025-10-31 18:39:31 -0600

    Agregando interfaz para gestion de clientes

---
+++ proyecto_final_desarrollo_web/proyecto_final_TBTECH/src/main/java/com/controller/GestionClientesController.java
@@ -0,0 +1,13 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class GestionClientesController {
+
+    @GetMapping("/clientes")
+    public String mostrarGestionClientes() {
+        return "clientes/clientes"; // solo para que thymeleaf busque al blog, no se ocupa ni service ni domain ni repo
+    }
+}
