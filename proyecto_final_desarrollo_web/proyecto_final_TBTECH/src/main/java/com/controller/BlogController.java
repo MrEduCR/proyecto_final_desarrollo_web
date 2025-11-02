62379e81b1d38bc85dcdd4d2f4153d14d4ed348b
MrEduCR~ <145067441+MrEduCR@users.noreply.github.com>
2025-11-01 22:09:03 -0600

    blog full funcional. Falta poner un logo mejorado al blog. Preguntarle al profe si podemos hacer el blog estatico por aparte.

--- proyecto_final_desarrollo_web/proyecto_final_TBTECH/src/main/java/com/controller/BlogController.java
+++ proyecto_final_desarrollo_web/proyecto_final_TBTECH/src/main/java/com/controller/BlogController.java
@@ -3,6 +3,8 @@ package com.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+// preguntar el profe si podemos manejar esto como una web estatica aparte luego
+
 @Controller
 public class BlogController {
 
@@ -10,4 +12,10 @@ public class BlogController {
     public String mostrarBlog() {
         return "blog/blog"; // solo para que thymeleaf busque al blog, no se ocupa ni service ni domain ni repo
     }
+
+    @GetMapping("/contacto")
+    public String mostrarContactoBlog() {
+        return "blog/contacto"; 
+    }
+    
 }
