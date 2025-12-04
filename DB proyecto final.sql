7c8dd692446fbd14a22120e7340d4e8197aeb05d
Eduardo Ramos <145067441+MrEduCR@users.noreply.github.com>
2025-12-03 19:06:29 -0600

    DB actualizada

--- DB proyecto final.sql
+++ DB proyecto final.sql
@@ -2,7 +2,6 @@ DROP DATABASE IF EXISTS terabyte_technology;
 CREATE DATABASE terabyte_technology CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 USE terabyte_technology;
 
-
 CREATE TABLE roles (
     rol_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     descripcion_rol VARCHAR(150) NOT NULL
@@ -12,6 +11,7 @@ CREATE TABLE usuarios (
     cedula_usuario INT PRIMARY KEY NOT NULL,
     nombre_usuario VARCHAR(150) NOT NULL,
     correo_usuario VARCHAR(150) NOT NULL UNIQUE,
+    contrasenia_usuario VARCHAR(100) NOT NULL,
     telefono_usuario VARCHAR(20) NOT NULL UNIQUE,
     rol_id INT NOT NULL,
     CONSTRAINT fk_usuarios_roles
@@ -26,16 +26,15 @@ CREATE TABLE clientes (
     correo_cliente VARCHAR(150) UNIQUE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-
 CREATE TABLE tipos_equipo (
     tipo_equipo_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     tipo_equipo_descripcion VARCHAR(50)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-
 CREATE TABLE tipos_servicio (
     tipo_servicio_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    tipo_servicio_descripcion VARCHAR(50)
+    tipo_servicio_descripcion VARCHAR(50),
+    precio_base DECIMAL(10,2) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE piezas (
@@ -86,6 +85,20 @@ CREATE TABLE servicios_piezas (
         ON DELETE RESTRICT ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+CREATE TABLE facturas (
+    factura_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    servicio_id INT NOT NULL,
+    observaciones_satisfaccion_usuario VARCHAR(500),
+    subtotal_piezas DECIMAL(10,2) NOT NULL DEFAULT 0,
+    subtotal_mano_obra DECIMAL(10,2) NOT NULL DEFAULT 0,
+    descuento_aplicado DECIMAL(10,2) NOT NULL DEFAULT 0,
+    impuestos_aplicados DECIMAL(10,2) NOT NULL DEFAULT 0,
+    total_final DECIMAL(10,2) NOT NULL DEFAULT 0,
+    fecha_emision DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_facturas_servicios
+        FOREIGN KEY (servicio_id) REFERENCES servicios(servicio_id)
+        ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE VIEW vista_servicios_con_piezas AS
 SELECT 
@@ -95,16 +108,6 @@ SELECT
     p.ruta_imagen
 FROM servicios_piezas sp
 INNER JOIN piezas p ON sp.pieza_id = p.pieza_id;
-
-CREATE TABLE facturas (
-    factura_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    servicio_id INT NOT NULL,
-    observaciones_satisfaccion_usuario VARCHAR(500),
-    CONSTRAINT fk_facturas_servicios
-        FOREIGN KEY (servicio_id) REFERENCES servicios(servicio_id)
-        ON DELETE RESTRICT ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
 
 CREATE OR REPLACE VIEW vista_facturas_detalle_completo AS
 SELECT
@@ -128,15 +131,16 @@ INNER JOIN clientes c ON s.cedula_cliente = c.cedula_cliente
 LEFT JOIN servicios_piezas sp ON s.servicio_id = sp.servicio_id
 LEFT JOIN piezas p ON sp.pieza_id = p.pieza_id;
 
+-- Insertar datos iniciales
 
 INSERT INTO roles (descripcion_rol) VALUES
-('Administrador'),
-('Técnico');
+('ADMIN'),
+('TECNICO');
 
-INSERT INTO usuarios (cedula_usuario, nombre_usuario, correo_usuario, telefono_usuario, rol_id) VALUES
-(1001, 'David Martínez', 'carlos.martinez@terabyte.com', '88888880', 1),
-(1002, 'Lucyna Gómez', 'laura.gomez@terabyte.com', '88888881', 1),
-(1003, 'Andrés Rojas', 'andres.rojas@terabyte.com', '88888882', 2);
+INSERT INTO usuarios (cedula_usuario, nombre_usuario, correo_usuario, contrasenia_usuario, telefono_usuario, rol_id) VALUES
+(1001, 'David Martínez', 'carlos.martinez@terabyte.com', '$2a$10$XJjc8A0w1qBmhifaCxOsHeV82LD0hjCkbt1VN6egD/7QJofuPLDeW','88888880', 1),
+(1002, 'Lucyna Gómez', 'laura.gomez@terabyte.com','$2a$10$XJjc8A0w1qBmhifaCxOsHeV82LD0hjCkbt1VN6egD/7QJofuPLDeW', '88888881', 1),
+(1003, 'Andrés Rojas', 'andres.rojas@terabyte.com','$2a$10$XJjc8A0w1qBmhifaCxOsHeV82LD0hjCkbt1VN6egD/7QJofuPLDeW', '88888882', 2);
 
 INSERT INTO clientes (cedula_cliente, nombre_cliente, telefono_cliente, correo_cliente) VALUES
 (2001, 'Juan Pérez', '1102223344', 'juan.perez@gmail.com'),
@@ -150,12 +154,12 @@ INSERT INTO tipos_equipo (tipo_equipo_descripcion) VALUES
 ('Tablet'),
 ('Teléfono móvil');
 
-INSERT INTO tipos_servicio (tipo_servicio_descripcion) VALUES
-('Mantenimiento preventivo'),
-('Reparación de hardware'),
-('Instalación de software'),
-('Diagnóstico'),
-('Actualización del sistema');
+INSERT INTO tipos_servicio (tipo_servicio_descripcion, precio_base) VALUES
+('Mantenimiento preventivo', 15000),
+('Reparación de hardware', 25000),
+('Instalación de software', 18000),
+('Diagnóstico', 12000),
+('Actualización del sistema', 20000);
 
 INSERT INTO piezas (pieza_descripcion, precio_pieza, ruta_imagen) VALUES
 ('Disco duro SSD 512GB', 32000.00, 'https://media.istockphoto.com/id/1410783528/es/foto/el-disco-ssd-m2-se-cierra-sobre-fondo-oscuro.jpg?s=2048x2048&w=is&k=20&c=r2PqYuCjB5pn4kCqWTBRudqTr1qOnUu0TjR3sLjp3Lc='),
@@ -180,8 +184,6 @@ INSERT INTO servicios (
 (2002, 2, 1, 3, 'SN-PC-20251028-002', '2025-10-20', 'https://i.ytimg.com/vi/5-CYF2Oy9GU/maxresdefault.jpg', 'Se quemo la cosa mop'),
 (2003, 3, 4, 1, 'SN-IMP-20251028-003', '2025-10-27', 'https://mediaserver.goepson.com/ImConvServlet/imconv/61dcb6a700968d5fe27870dc9e72d7151805d623/1200Wx1200H', 'Impresora no imprime, posible atasco de papel');
 
-INSERT INTO facturas (servicio_id, observaciones_satisfaccion_usuario) VALUES
-(1, 'Cliente satisfecho poorque el equipo esta funcionando correctamente.'),
-(2, 'El cliente agradeció el mantenimiento.'),
-(3, 'Cliente feliz y ya.');
-
+INSERT INTO servicios_piezas (
+    servicio_id, pieza_id
+) VALUES (1,2),(1,3),(2,2),(3,3);
